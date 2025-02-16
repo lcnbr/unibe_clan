f69b709884176acc6eddf999073e93501b8d07bf
lcnbr <im@lcnbr.ch>
2025-02-16 15:53:10 +0100

    fixed, flake check passes

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "Unibe Clan";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # New flake-parts input
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    impermanence.url = "github:nix-community/impermanence";
+    clan-core = {
+      url = "git+https://git.clan.lol/clan/clan-core";
+      inputs.nixpkgs.follows = "nixpkgs"; # Needed if your configuration uses nixpkgs unstable.
+      # New
+      inputs.flake-parts.follows = "flake-parts";
+    };
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    clan-core,
+    impermanence,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} ({
+      self,
+      pkgs,
+      ...
+    }: {
+      # We define our own systems below. you can still use this to add system specific outputs to your flake.
+      # See: https://flake.parts/getting-started
+      systems = ["x86_64-linux"];
+
+      # import clan-core modules
+      imports = [
+        clan-core.flakeModules.default
+      ];
+      # Define your clan
+      # See: https://docs.clan.lol/reference/nix-api/buildclan/
+      clan = {
+        # Clan wide settings. (Required)
+        meta.name = "Unibe clan"; # Ensure to choose a unique name.
+
+        machines = {
+          itppeach = {
+            imports = [
+              ./machines/itppeach/configuration.nix
+              ./modules/disko.nix
+              impermanence.nixosModules.impermanence
+            ];
+            nixpkgs.hostPlatform = "x86_64-linux";
+
+            # Set this for clan commands use ssh i.e. `clan machines update`
+            clan.core.networking.targetHost = pkgs.lib.mkDefault "root@itppeach";
+
+            # There needs to be exactly one controller per clan
+            clan.core.networking.zerotier.controller.enable = true;
+          };
+        };
+      };
+
+      perSystem = {
+        lib,
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            clan-core.packages.${system}.clan-cli
+            pkgs.nil
+            pkgs.nixd
+            pkgs.alejandra
+          ];
+        };
+      };
+    });
+}
---
+++ machines/itppeach/configuration.nix
@@ -0,0 +1,106 @@
+{lib,pkgs,...}:{
+  imports = [
+    # contains your disk format and partitioning configuration.
+    ../../modules/disko.nix
+    # this file is shared among all machines
+    ../../modules/shared.nix
+    # enables GNOME desktop (optional)
+    # ../../modules/gnome.nix
+  ];
+
+  # This is your user login name.
+  users.users.user.name = "lcnbr";
+
+  # Set this for clan commands use ssh i.e. `clan machines update`
+  # If you change the hostname, you need to update this line to root@<new-hostname>
+  # This only works however if you have avahi running on your admin machine else use IP
+  clan.core.networking.targetHost = "root@130.92.184.147";
+
+  networking.hostName = "itppeach";
+  networking.hostId="aaaa3453";
+
+  fileSystems."/persist" =
+    {
+    neededForBoot=true;
+    device = "zroot/local/persist";
+      fsType = "zfs";
+    };
+
+  # boot.initrd.postResumeCommands = lib.mkAfter ''
+  #    zfs rollback -r zroot/local/root@blank && echo "rollback complete"
+  #  '';
+
+   boot.initrd.systemd.enable = true;
+
+   boot.initrd.systemd.services.reset = {
+       description = "reset root filesystem";
+       wantedBy = [ "initrd.target" ];
+       after = [ "zfs-import-zroot.service" ];
+       before = [ "sysroot.mount" ];
+       path = with pkgs; [ zfs ];
+       unitConfig.DefaultDependencies = "no";
+       serviceConfig.Type = "oneshot";
+       script = ''
+           zfs rollback -r zroot/local/root@blank'';
+     };
+
+   environment.persistence."/persist" = {
+       enable = true;  # NB: Defaults to true, not needed
+       hideMounts = true;
+       directories = [
+         "/var/log"
+         "/var/lib/bluetooth"
+         "/var/lib/nixos"
+         "/var/lib/systemd/coredump"
+         "/etc/NetworkManager/system-connections"
+         { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+       ];
+       users.lcnbr = {
+         directories = [
+           "documents"
+           "media"
+           "dev"
+           { directory = ".gnupg"; mode = "0700"; }
+           { directory = ".ssh"; mode = "0700"; }
+           { directory = ".nixops"; mode = "0700"; }
+           { directory = ".local/share/keyrings"; mode = "0700"; }
+           ".local/share/direnv"
+         ];
+         files = [
+           ".screenrc"
+         ];
+       };
+     };
+
+  # You can get your disk id by running the following command on the installer:
+  # Replace <IP> with the IP of the installer printed on the screen or by running the `ip addr` command.
+  # ssh root@<IP> lsblk --output NAME,ID-LINK,FSTYPE,SIZE,MOUNTPOINT
+  disko.devices.disk.main.device = "/dev/disk/by-id/wwn-0x55cd2e41503ed9cb";
+
+
+
+  users.users = {
+    root={
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILwU7t6UJmWcDd+oayloWbTNixqe5J010amkU0p/7gKc im@lcnbr.ch"
+      ];
+      # initialHashedPassword="$6$1EKwWplF7X6IP7d4$hcpJVomZ4k0LH8lpnNjkgcYJwciDh/fvcOo0/fSrg/z/VT.DQjN4weLg3gtZI4wniETjeycJbQAu6ElTBqFyN0";
+    };
+    lcnbr = {
+      isNormalUser = true;
+      # initialHashedPassword="$6$1EKwWplF7X6IP7d4$hcpJVomZ4k0LH8lpnNjkgcYJwciDh/fvcOo0/fSrg/z/VT.DQjN4weLg3gtZI4wniETjeycJbQAu6ElTBqFyN0";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILwU7t6UJmWcDd+oayloWbTNixqe5J010amkU0p/7gKc im@lcnbr.ch"
+      ];
+      extraGroups = ["wheel" "networkmanager"];
+    };
+  };
+
+
+  # Zerotier needs one controller to accept new nodes. Once accepted
+  # the controller can be offline and routing still works.
+  clan.core.networking.zerotier.controller.enable = true;
+
+
+  system.stateVersion = "25.05";
+}
